5d572e97b824a1298b760d319dacb06f393a8797
Jorge Ivan Belalcazar <jorge.belalcazar@correounivalle.edu.co>
2026-06-27 16:03:11 -0500

    feat: add App entry point with ViewManager for robust stage loading

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,13 @@
 module com.example.cincuentazo {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires javafx.graphics;
 
     opens com.example.cincuentazo to javafx.fxml;
     exports com.example.cincuentazo;
+
+    opens com.example.cincuentazo.controller to javafx.fxml;
+    exports com.example.cincuentazo.controller;
+
+    exports com.example.cincuentazo.view;
 }
